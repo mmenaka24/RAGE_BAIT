3a00d2eebf88beae4455a06b0224b24a8b920a3e
Menaka Maruthainar <menaka.maruthainar@softwire.com>
2025-08-13 23:05:38 +0100

    refactor(fish): Change fish movement to velocity-based

--- fish.gd
+++ fish.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
-@export var move_speed: int = 10
-var target_position: Vector2 = Vector2.ZERO
+@export var max_move_speed: int = 100
+@export var change_direction_time: float = 1.0 # seconds before picking a new direction
+
+var velocity: Vector2 = Vector2.ZERO
 var lake_polygon: PackedVector2Array
+var direction_timer: float = 0.0
+
+var move_speed: int = 40
 
 func _ready() -> void:
 	var lake_area: Area2D = get_tree().get_root().find_child("LakeArea", true, false) as Area2D
@@ -13,24 +18,24 @@ func _ready() -> void:
 	for p: Vector2 in collision_poly.polygon:
 		lake_polygon.append(collision_poly.to_global(p))
 	
-	set_random_target()
+	pick_new_direction()
 
 func _process(delta: float) -> void:
-	var direction: Vector2 = (target_position - position)
-	if direction.length() < 5:
-		set_random_target()
+	direction_timer -= delta
+	if direction_timer <= 0:
+		pick_new_direction()
+	
+	var new_position: Vector2 = global_position + velocity * delta
+	if Geometry2D.is_point_in_polygon(new_position, lake_polygon):
+		global_position = new_position
 	else:
-		var new_position: Vector2 = global_position + direction.normalized() * move_speed * delta
-		if Geometry2D.is_point_in_polygon(new_position, lake_polygon):
-			global_position = new_position
-		else:
-			set_random_target()
+		pick_new_direction()
 
-func set_random_target() -> void:
-	var tries: int = 0
-	while tries < 100:
-		var candidate_target_position: Vector2 = lake_polygon[int(randf() * lake_polygon.size())]
-		if Geometry2D.is_point_in_polygon(candidate_target_position, lake_polygon):
-			target_position = candidate_target_position
-			return
-		tries += 1
+func set_speed() -> void:
+	move_speed = randi_range(10, max_move_speed)
+
+func pick_new_direction() -> void:
+	# pick a random unit direction
+	var angle: float = randf() * TAU
+	velocity = Vector2(cos(angle), sin(angle)) * move_speed
+	direction_timer = randf_range(0.01, change_direction_time)
