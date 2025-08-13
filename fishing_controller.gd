17332b63195b13e02b2f7bd86bbda46414e6cdfc
Menaka Maruthainar <menaka.maruthainar@softwire.com>
2025-08-13 21:33:07 +0100

    basic fishing game works yayyyy

--- fishing_controller.gd
+++ fishing_controller.gd
@@ -1,21 +1,21 @@
 extends Node2D
 
-@export var reel_rate: int = 3    # how fast you pull fish in
-@export var lose_rate: int = 2    # how fast you lose progress
+@export var reel_rate: int = 30    # how fast you pull fish in
+@export var lose_rate: int = 20    # how fast you lose progress
+
 var catching: bool = false
+var fishing_started: bool = false
 
 var catch_meter: TextureProgressBar
 var hook: Area2D
 var fish: Area2D
-
-var fishing_started: bool = false
 
 func _ready() -> void:
 	catch_meter = $CatchMeter as TextureProgressBar
 	hook = $Player/Hook as Area2D
 	fish = $Fish as Area2D
 	catch_meter.max_value = 100
-	catch_meter.value = 10
+	catch_meter.value = 100
 
 	hook.connect("area_entered", Callable(self, "_on_hook_area_entered"))
 	hook.connect("area_exited", Callable(self, "_on_hook_area_exited"))
@@ -34,9 +34,8 @@ func _process(delta: float) -> void:
 
 	if catch_meter.value <= 0:
 		print("You caught the fish!")
+		fishing_started = false
 		# TODO: win animation or reset
-		
-	print(catch_meter.value)
 
 func _on_hook_area_entered(area: Area2D) -> void:
 	if area == fish:
@@ -45,5 +44,3 @@ func _on_hook_area_entered(area: Area2D) -> void:
 func _on_hook_area_exited(area: Area2D) -> void:
 	if area == fish:
 		catching = false
-		
-# Currently catch meter isn't actually detecting that fish & hook are overlapping - need to fix this
